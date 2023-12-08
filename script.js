c0bfa44ec332c65fca2c79298b533bf964a89f67
Kareem Ellis <kareemellis55@gmail.com>
2023-12-08 00:28:15 -0500

    Fixed modal code

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 window.addEventListener('load', (event) => {
+    const body = document.body;
     const modal = document.getElementById('myModal');
     const openButtons = document.querySelectorAll('.openModal');
     const closeButton = document.getElementById('closeModal');
+    let scrollPosition = 0;
+
+    function getScrollTop() {
+        return window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    }
 
     openButtons.forEach((el) => {
         el.addEventListener('click', () => {
+            scrollPosition = getScrollTop(); // Store the scroll position
+            body.style.overflow = 'hidden';
+            body.style.position = 'fixed';
+            body.style.top = `-${scrollPosition}px`;
             modal.showModal();
+            //body.classList.add('no-scroll');
         })
     })
     // openButton.onclick = function() {
@@ -14,12 +25,22 @@ window.addEventListener('load', (event) => {
 
     closeButton.onclick = function() {
         modal.close(); // Use the native close() method
+        //body.classList.remove('no-scroll');
+        body.style.removeProperty('overflow');
+        body.style.removeProperty('position');
+        body.style.removeProperty('top');
+        window.scrollTo(0, scrollPosition);
     }
 
     // Optional: Close the dialog when clicking outside of it
     window.onclick = function(event) {
         if (event.target === modal) {
             modal.close();
+            //body.classList.remove('no-scroll');
+            body.style.removeProperty('overflow');
+            body.style.removeProperty('position');
+            body.style.removeProperty('top');
+            window.scrollTo(0, scrollPosition);
         }
     }
 
