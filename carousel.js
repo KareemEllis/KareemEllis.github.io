83bf5c7154e965f94ed69fcc828b431b78030773
Kareem Ellis <kareemellis55@gmail.com>
2024-03-16 12:13:40 -0500

    Added Jotter Project Carousel

--- carousel.js
+++ carousel.js
@@ -1,25 +1,30 @@
+
 document.addEventListener("DOMContentLoaded", function() {
+  // Initialize the carousels
+  const carousels = document.querySelectorAll(".carousel-container");
+  carousels.forEach(function(carousel) {
+    const slides = carousel.querySelector(".carousel").getElementsByTagName("img");
     // Hide all images except the first one
-    const slides = document.getElementsByClassName("carousel")[0].getElementsByTagName("img");
     for (let i = 1; i < slides.length; i++) {
       slides[i].style.display = "none";
     }
   });
-  
-  let slideIndex = 0;
-  
-  function moveSlide(n) {
-    const slides = document.getElementsByClassName("carousel")[0].getElementsByTagName("img");
-    slideIndex += n;
-    if (slideIndex >= slides.length) {
-      slideIndex = 0;
-    }
-    if (slideIndex < 0) {
-      slideIndex = slides.length - 1;
-    }
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slides[slideIndex].style.display = "block";
+});
+
+function moveSlide(n, carouselIndex) {
+  const carousels = document.querySelectorAll(".carousel-container");
+  const slides = carousels[carouselIndex].querySelector(".carousel").getElementsByTagName("img");
+  let slideIndex = parseInt(carousels[carouselIndex].getAttribute("data-slide-index"));
+  slideIndex += n;
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
   }
-  
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex].style.display = "block";
+  carousels[carouselIndex].setAttribute("data-slide-index", slideIndex);
+}
